56bc4bba7cb3dc51fe6746a4b51bc41aa2f90e79
azam1919 <azamnaveed1919@gmail.com>
2022-10-09 11:57:24 +0500

    remove unwante code from tutorial code

--- resources/views/admin/web-training/tutorial/create.blade.php
+++ resources/views/admin/web-training/tutorial/create.blade.php
@@ -8,7 +8,6 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Web Training | Create Tutorials</title>
-    {{-- @Include('layouts.favicon') --}}
     @Include('layouts.links.admin.head')
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <link href="https://releases.transloadit.com/uppy/v3.0.1/uppy.min.css" rel="stylesheet">
@@ -49,135 +48,129 @@
 <body class="hold-transition sidebar-mini layout-fixed">
     @extends('layouts.admin.master')
     @section('content')
-        <div class="wrapper">
-            <!-- Content Wrapper. Contains page content -->
-            <div class="content-wrapper">
-                <!-- Content Header (Page header) -->
-                <div class="content-header">
-                    <div class="container-fluid">
-                        @if (session('success'))
-                            <div id="success" class="alert alert-default-success alert-dismissible fade show"
-                                role="alert">
-                                <strong>{{ session('success') }}</strong>
-                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                        @endif
-                        <div class="row mb-2">
-                            <div class="col-sm-6">
-                                <h1 class="m-0">Create {{ $heading['0']->heading }} Tutorials</h1>
-                            </div>
-                            <div class="col-sm-6">
-                                <ol class="breadcrumb float-sm-right">
-                                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                    <li class="breadcrumb-item active">Create Tutorials</li>
-                                </ol>
-                            </div>
+    <div class="wrapper">
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    @if (session('success'))
+                    <div id="success" class="alert alert-default-success alert-dismissible fade show" role="alert">
+                        <strong>{{ session('success') }}</strong>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    @endif
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Create {{ $heading['0']->heading }} Tutorials</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">Create Tutorials</li>
+                            </ol>
                         </div>
                     </div>
                 </div>
-                <!-- /.content-header -->
-                <!-- Main content -->
-                <link rel="stylesheet" href="{{ asset('dist/css/imageupload/fancy_fileupload.css') }}">
-                <section class="content">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <section class="col-lg-5 connectedSortable">
-                                <!-- Custom tabs with image drag and drop -->
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h3 class="card-title">
-                                            <i class="fas fa-image mr-1"></i>
-                                            Images
-                                        </h3>
-                                    </div>
-                                    <div class="card-body">
-                                        <div class="p-0">
-                                            <div class="container-fluid">
-                                                <div class="row">
-                                                    <div class="col-lg-12"
-                                                        style="height: 350px; overflow: hidden; overflow-y: scroll;">
-                                                        <div id="drag-drop-area"></div>
-                                                        <!-- <div class="fallback">
+            </div>
+            <!-- /.content-header -->
+            <!-- Main content -->
+            <link rel="stylesheet" href="{{ asset('dist/css/imageupload/fancy_fileupload.css') }}">
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <section class="col-lg-5 connectedSortable">
+                            <!-- Custom tabs with image drag and drop -->
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="card-title">
+                                        <i class="fas fa-image mr-1"></i>
+                                        Images
+                                    </h3>
+                                </div>
+                                <div class="card-body">
+                                    <div class="p-0">
+                                        <div class="container-fluid">
+                                            <div class="row">
+                                                <div class="col-lg-12" style="height: 350px; overflow: hidden; overflow-y: scroll;">
+                                                    <div id="drag-drop-area"></div>
+                                                    <!-- <div class="fallback">
                                                                                                                                                                                                                                                                                     <form action="" id="formdata">
                                                                                                                                                                                                                                                                                         <input id="fancy_upload" type="file" name="fancy_upload"
                                                                                                                                                                                                                                                                                             accept=".jpg, .png, image/jpeg, image/png" multiple>
                                                                                                                                                                                                                                                                                     </form>
                                                                                                                                                                                                                                                                                 </div> -->
-                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <!-- /.card -->
-                                <!-- Uploaded images -->
-                                <div class="card">
-                                    <div class="card-header">
-                                        <h3 class="card-title">
-                                            <i class="fas fa-image mr-1"></i>
-                                            Uploaded Images
-                                        </h3>
-                                    </div>
-                                    <div class="card-body">
-                                        <div class="tab-content p-0">
-                                            <div class="chart " id="revenue-chart"
-                                                style="position: relative; height: 315px; overflow-y: scroll;">
-                                            </div>
+                            </div>
+                            <!-- /.card -->
+                            <!-- Uploaded images -->
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="card-title">
+                                        <i class="fas fa-image mr-1"></i>
+                                        Uploaded Images
+                                    </h3>
+                                </div>
+                                <div class="card-body">
+                                    <div class="tab-content p-0">
+                                        <div class="chart " id="revenue-chart" style="position: relative; height: 315px; overflow-y: scroll;">
                                         </div>
                                     </div>
                                 </div>
-                            </section>
-                            <!-- /.Left col -->
-                            <!-- right col (We are only adding the ID to make the widgets sortable)-->
-                            <section class="col-lg-7 connectedSortable">
-                                <form action="" method="post">
-                                    <!-- Edit Image Section -->
-                                    <div class="card ">
-                                        <div class="card-header">
-                                            <h3 class="card-title">
-                                                <i class="fas fa-edit mr-1"></i>
-                                                Edit Images
-                                            </h3>
-                                        </div>
-                                        <div class="card-body">
-                                            <div style="height: 346px; width: 100%;">
-                                                @include('layouts.links.admin.tutorial.jcrop')
-                                                <img src="{{ asset('dist/img/edit profile.jpg') }}" alt="image"
-                                                    class="img-fluid" id="image"
-                                                    style="height: 346px; width: 100%;  object-fit: cover;">
-                                                <script src="/dist/js/tutorial/jcrop.js"></script>
-                                            </div>
+                            </div>
+                        </section>
+                        <!-- /.Left col -->
+                        <!-- right col (We are only adding the ID to make the widgets sortable)-->
+                        <section class="col-lg-7 connectedSortable">
+                            <form action="" method="post">
+                                <!-- Edit Image Section -->
+                                <div class="card ">
+                                    <div class="card-header">
+                                        <h3 class="card-title">
+                                            <i class="fas fa-edit mr-1"></i>
+                                            Edit Images
+                                        </h3>
+                                    </div>
+                                    <div class="card-body">
+                                        <div style="height: 346px; width: 100%;">
+                                            @include('layouts.links.admin.tutorial.jcrop')
+                                            <img src="{{ asset('dist/img/edit profile.jpg') }}" alt="image" class="img-fluid" id="image" style="height: 346px; width: 100%;  object-fit: cover;">
+                                            <script src="/dist/js/tutorial/jcrop.js"></script>
                                         </div>
                                     </div>
-                                    <!-- /.card -->
-                                    <!-- Description card -->
-                                    <div class="card">
-                                        <div class="card-header ">
-                                            <h3 class="card-title">
-                                                <i class="fas fa-edit mr-1"></i>
-                                                Description
-                                            </h3>
-                                        </div>
-                                        <div class="card-body" id="summernote">
-                                            <div style="height: 250px; width: 100%;">
-                                            </div>
+                                </div>
+                                <!-- /.card -->
+                                <!-- Description card -->
+                                <div class="card">
+                                    <div class="card-header ">
+                                        <h3 class="card-title">
+                                            <i class="fas fa-edit mr-1"></i>
+                                            Description
+                                        </h3>
+                                    </div>
+                                    <div class="card-body" id="summernote">
+                                        <div style="height: 250px; width: 100%;">
                                         </div>
                                     </div>
-                                    <!-- /.card -->
-                                </form>
-                            </section>
-                            <!-- right col -->
-                        </div>
+                                </div>
+                                <!-- /.card -->
+                            </form>
+                        </section>
+                        <!-- right col -->
                     </div>
-                </section>
+                </div>
+            </section>
 
-            </div>
         </div>
+    </div>
     @endsection
     @Include('layouts.links.admin.foot')
-    {{-- <!-- <script src="{{ asset('dist/js/imageupload/jquery-1.12.4.min.js') }}"></script> --> --}}
     <script src="https://transloadit.edgly.net/releases/uppy/v1.6.0/uppy.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="{{ asset('dist/js/imageupload/jquery.ui.widget.js') }}"></script>
@@ -187,67 +180,24 @@
     <script src="{{ asset('dist/js/pages/tutorial/summernote-lite.min.js') }}"></script>
     <script src="{{ asset('dist/js/pages/tutorial/summer-note.js') }}"></script>
     <script type="module">
-      import {Uppy, Dashboard, Tus} from "https://releases.transloadit.com/uppy/v3.0.1/uppy.min.mjs"
-      var uppy = new Uppy()
-        .use(Dashboard, {
-          inline: true,
-          target: '#drag-drop-area'
-        })
-        .use(Tus, {
-            endpoint: "{{ route('tutorial.create.store') }}",
-            formData: true,
-            fieldName: 'files[]',
-            headers: {
-    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-},
-// autoRetry: false,
-        })
+        import {
+            Uppy,
+            Dashboard,
+            Tus
+        } from "https://releases.transloadit.com/uppy/v3.0.1/uppy.min.mjs"
+        var uppy = new Uppy()
+            .use(Dashboard, {
+                inline: true,
+                target: '#drag-drop-area'
+            })
+            .use(Tus, {
+                endpoint: "{{ route('tutorial.create.store') }}",
+            })
 
-      uppy.on('complete', (result) => {
-        console.log('Upload complete! We’ve uploaded these files:', result.successful)
-      })
+        uppy.on('complete', (result) => {
+            console.log('Upload complete! We’ve uploaded these files:', result.successful)
+        })
     </script>
-    <!-- <script>
-        $(document).ready(function() {
-            $('#fancy_upload').FancyFileUpload({
-                // send data to this url
-                'url': "{{ route('tutorial.create.store') }}",
-
-                // key-value pairs to send to the server
-                'params': {
-                    _token: $('#formdata').find('input[name="_token"]').first().val(),
-                },
-
-                // editable file name?
-                'edit': true,
-
-                // max file size
-                'maxfilesize': -1,
-
-
-                // the number of retries to perform before giving up
-                'retries': 0,
-
-                // A valid callback function that is called during initialization to allow for last second changes to the settings.
-                // Useful for altering fileupload options on the fly.
-                'preinit': null,
-
-                // A valid callback function that is called at the end of initialization of each instance.
-                'postinit': null,
-
-                // called for each item after it has been added to the DOM
-                'added': function(e, data) {
-                    // do something
-                },
-
-                // called whenever starting the upload
-                'startupload': function(SubmitUpload, e, data) {
-                    // do something
-                }
-
-            });
-        });
-    </script> -->
 
 </body>
 
