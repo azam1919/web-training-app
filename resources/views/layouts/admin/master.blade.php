31083c0aa65b2cdd63a892386be5a7dabca3df21
M-Arslan <arslan_junior@eniture-dev.com>
2022-10-15 16:57:27 +0500

    upper navbar notification and messages remove

--- resources/views/layouts/admin/master.blade.php
+++ resources/views/layouts/admin/master.blade.php
@@ -40,14 +40,13 @@
           </li>
 
           <!-- Messages Dropdown Menu -->
-          <li class="nav-item dropdown">
+          <!-- <li class="nav-item dropdown">
               <a class="nav-link" data-toggle="dropdown" href="#">
                   <i class="far fa-comments"></i>
                   <span class="badge badge-danger navbar-badge">3</span>
               </a>
               <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
                   <a href="#" class="dropdown-item">
-                      <!-- Message Start -->
                       <div class="media">
                           <img src="{{ asset('dist/img/user1-128x128.jpg') }}" alt="User Avatar"
                               class="img-size-50 mr-3 img-circle">
@@ -60,11 +59,9 @@
                               <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
                           </div>
                       </div>
-                      <!-- Message End -->
                   </a>
                   <div class="dropdown-divider"></div>
                   <a href="#" class="dropdown-item">
-                      <!-- Message Start -->
                       <div class="media">
                           <img src="{{ asset('dist/img/user8-128x128.jpg') }}" alt="User Avatar"
                               class="img-size-50 img-circle mr-3">
@@ -77,11 +74,9 @@
                               <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
                           </div>
                       </div>
-                      <!-- Message End -->
                   </a>
                   <div class="dropdown-divider"></div>
                   <a href="#" class="dropdown-item">
-                      <!-- Message Start -->
                       <div class="media">
                           <img src="{{ asset('dist/img/user3-128x128.jpg') }}" alt="User Avatar"
                               class="img-size-50 img-circle mr-3">
@@ -94,14 +89,13 @@
                               <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
                           </div>
                       </div>
-                      <!-- Message End -->
                   </a>
                   <div class="dropdown-divider"></div>
                   <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
               </div>
-          </li>
+          </li> -->
           <!-- Notifications Dropdown Menu -->
-          <li class="nav-item dropdown">
+          <!-- <li class="nav-item dropdown">
               <a class="nav-link" data-toggle="dropdown" href="#">
                   <i class="far fa-bell"></i>
                   <span class="badge badge-warning navbar-badge">15</span>
@@ -126,7 +120,7 @@
                   <div class="dropdown-divider"></div>
                   <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
               </div>
-          </li>
+          </li> -->
           <li class="nav-item">
               <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                   <i class="fas fa-expand-arrows-alt"></i>
